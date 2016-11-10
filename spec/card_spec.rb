d84f497e6b8778c06084d5358be21e073e26879f
William Mantegna <wmantegna@gmail.com>
2016-11-10 11:20:18 -0500

    getting started -- writing compact specifications 2 (shared examples)

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -38,12 +38,8 @@ describe Card do
         raise unless Set.new([subject, other]).size == 1
       end
     end
-
-    describe 'comparing to a card of different suit' do
-      def other
-        @other ||= card(suit: :hearts, rank: 4)
-      end
-
+    
+    shared_examples_for 'an unequal card' do
       it 'is not equal' do
         raise unless subject != other
       end
@@ -52,17 +48,20 @@ describe Card do
       end
     end
 
+    describe 'comparing to a card of different suit' do
+      def other
+        @other ||= card(suit: :hearts, rank: 4)
+      end
+
+      it_behaves_like 'an unequal card'
+    end
+
     describe 'comparing to a card of different rank' do
       def other
         @other ||= card(suit: :spades, rank: 5)
       end
 
-      it 'is not equal' do
-        raise unless subject != other
-      end
-      it 'is not hash equal' do
-        raise unless Set.new([subject, other]).size == 2 
-      end
+      it_behaves_like 'an unequal card'
     end
   end
 
